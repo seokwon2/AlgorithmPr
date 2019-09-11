a89b4f9101b4da0f9bc33fd0b0c9b04276e6bcc3
pam94 <pam94@DESKTOP-FJPUT35>
2019-09-11 21:32:55 +0900

    기본 알고리즘

--- src/beginner/Algorithm_B_01.java
+++ src/beginner/Algorithm_B_01.java
@@ -1,8 +1,16 @@
 package beginner;
 
+//기본1. 1~100까지 합계 구하기
 public class Algorithm_B_01 {
 	public static void main(String[] ar) {
-			// kkkk
-		
+
+		int i=0; // 1씩 증가하는 숫자
+		int j=0; // 누적 값을 담을 변수
+
+		do {
+			++i;
+			j+=i;
+		} while (i < 100);
+		System.out.println(j); //누적값을 담고 있는 j를 출력
 	}
 }
