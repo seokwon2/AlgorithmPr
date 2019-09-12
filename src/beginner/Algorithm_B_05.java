01ce9327576543b7690982de6fc0e61dbc4b0bf9
pam94 <pam94@DESKTOP-FJPUT35>
2019-09-12 11:37:27 +0900

    주석 추가

--- src/beginner/Algorithm_B_05.java
+++ src/beginner/Algorithm_B_05.java
@@ -1,16 +1,19 @@
 package beginner;
+
 //홀수 짝수 별로 부호 변경하면서 곱하기
 public class Algorithm_B_05 {
 	public static void main(String[] ar) {
-		int i; double j;
+		int i;
+		double j;
 		j = 1;
 
 		for (i = 1; i < 100; i++) {
 			if (i % 2 == 0) { // i 값이 짝수 값인지 확인
-				j = j * i; //짝수일때는 그냥 곱해서 j에 누적
+				j = j * i; // 짝수일때는 그냥 곱해서 j에 누적
 			} else {
-				j *= i * -1; //홀수 일때는 음수값을 j에 연산후 누적
+				j *= i * -1; // 홀수 일때는 음수값을 j에 연산후 누적
 			}
+
 		}
 		System.out.println(j);
 	}
