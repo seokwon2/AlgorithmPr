4eb3cb7b3f5510c66ea4732fd1528a767d692ee0
pam94 <pam94@DESKTOP-FJPUT35>
2019-09-15 19:05:31 +0900

    code, 주석 변경

--- src/beginner/Algorithm_B_10.java
+++ src/beginner/Algorithm_B_10.java
@@ -8,16 +8,19 @@ public class Algorithm_B_10 {
 		System.out.println("숫자를 입력하시오");
 		int Input = scan.nextInt();
 
-		int i = Input - 1; // 자기 자신 -1 까지 나누면서 나머지확인
-		int j = 2; // 나눌값중에 최소값인 2를 저장
+		int i = Input; // 
 
-		for (int k = j; k <= i; k++) {
-			if (Input % k == 0) {
-				System.out.println("소수 아님");
-				break; //나머지 연산을 할때 2로 나누었을때 나누어 떨어지는 수라면 소수가 아니라고 판단
-						//break 사용
-			}else{
-				System.out.println("소수");
+		for (int k = 2; k <= i; k++) {
+			if (Input % k == 0) { // 나머지가 0인지 확인
+				if (Input == k) { // 입력받은 값과  나머지가 0이 나오게 한 k값을 확인해서 그수가 같다면 소수
+					System.out.println("소수");
+					break;
+				} else {
+					System.out.println("소수 아님");
+					break; // 나머지 연산을 할때 2로 나누었을때 나누어 떨어지는 수라면 소수가 아니라고 판단
+							// break 사용
+				}
+
 			}
 
 		}
